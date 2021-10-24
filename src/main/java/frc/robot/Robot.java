3462fa2e88fde14918d44e035816ba78c0dce5d2
SidKam11 <kamath.siddarth@gmail.com>
2021-10-23 18:57:42 -0700

    Involving PID into Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,9 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -24,10 +27,18 @@ public class Robot extends TimedRobot {
   private static final int kMotorPort = 7;
   private static final int kJoystickPort = 0;
   private static final int kUltrasonicPort = 0;
+  private static final int ktarget = 10;
+  private static final double kP = 0.00005;
+  private static final double kI = 0.0001;
+  private static final double kD = 0.00001;
+
+  final int kUnitsPerRevolution = 2048;
+  double errorSum = 0;
+  double prevError = ktarget * kUnitsPerRevolution;
 
   // Initialize objects 
   private XboxController joystick = new XboxController(kJoystickPort); // Joystick
-  private final WPI_TalonFX falcon = new WPI_TalonFX(10); // TalonFX
+  private final WPI_TalonFX talon = new WPI_TalonFX(10); // TalonFX
   private final AnalogInput ultrasonic = new AnalogInput(kUltrasonicPort); // Ultrasonic sensor
 
   /**
@@ -36,28 +47,62 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+
     
     // Reset TalonFX & perform any needed clean up
-    falcon.configFactoryDefault();
 
+		/* newer config API */
+		TalonFXConfiguration configs = new TalonFXConfiguration();
+		/* select integ-sensor for PID0 (it doesn't matter if PID is actually used) */
+		configs.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor;
+		/* config all the settings */
+    talon.configAllSettings(configs);
+    talon.setSelectedSensorPosition(0);
   }
 
   @Override
   public void teleopPeriodic() {
+    double selSenPos = talon.getSelectedSensorPosition(0); /* position units */
+    double selSenVel = talon.getSelectedSensorVelocity(0); /* position units per 100ms */
+
+    double pos_Rotations = (double) selSenPos / kUnitsPerRevolution;
+		double vel_RotPerSec = (double) selSenVel / kUnitsPerRevolution * 10; /* scale per100ms to perSecond */
+    double vel_RotPerMin = vel_RotPerSec * 60.0;
+    
+    double error = ktarget * kUnitsPerRevolution - selSenPos;
+    double deltaError = (error - prevError) / 0.02;
+    double PIDmultiplier = (errorSum * kI) + (error * kP) + (deltaError * kD);
+    
+    prevError = error;
 
     // Set speed of motors (value between [-1,1]) by
     // taking input from joystick (value also between [-1, 1])
     // and scaling it down (so things don't break)
     // and using that value as the input
-    falcon.set(TalonFXControlMode.PercentOutput, joystick.getRawAxis(1) * 0.1);
-
     // Get value from ultrasonic (12-bit value) and
     // convert to inches
     double distance = ultrasonic.getValue() * 0.0492;
+    
+    if(error / kUnitsPerRevolution < 1)
+    {
+      errorSum += error * 0.02;
+    }
+
+
+    if(PIDmultiplier > 0.1)
+    {
+      talon.set(TalonFXControlMode.PercentOutput, 0.1);
+    } else {
+      talon.set(TalonFXControlMode.PercentOutput, PIDmultiplier);
+    }
 
     // Display distance on Smart Dashboard & print
     SmartDashboard.putNumber("Distance", distance);
-    // System.out.println("Distance: " + distance);
+    SmartDashboard.putNumber("Pos-units: ", selSenPos);
+    SmartDashboard.putNumber("Vel-unitsPer100ms: ", selSenVel);
+    SmartDashboard.putNumber("Pos-Rotations: ", pos_Rotations);
+    SmartDashboard.putNumber("Vel-RPM: ", vel_RotPerMin);
+    SmartDashboard.putNumber("Vel-RPS: ", vel_RotPerSec);
   }
 
 
